09414a6b81553181779b2ac591dfdd776b8b3936
93Laer <874599481@qq.com>
2018-03-27 15:12:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { AppRegistry } from 'react-native';
-const  app_key='MyDemo';//
-//todo ��дHelloWorld
+const  app_key='MyDemo';//这个名字必须和我们工程的名字要一样
+
 
 /*import App from './App';
 AppRegistry.registerComponent('MyDemo', () => App);*/
 
-//todo ʹ��Image
+
 import HomePager from './HomePager.js'
 AppRegistry.registerComponent(app_key,()=>HomePager)
 
